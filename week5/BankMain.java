47087e80863e9f251a12c2431267d54d989505ee
Meiza Reksa Fahrevi <fnoor6612@gmail.com>
2026-03-09 08:30:09 +0700

    main

--- week5/BankMain.java
+++ week5/BankMain.java
@@ -4,13 +4,13 @@ public class BankMain {
     public static void main(String[] args) {
         System.out.println("=== Bank Account===");
 
-        BankAcccount akun1 = new BankAcccount("Reksa", 500);
+        BankAcccount akun1 = new BankAcccount("Reksa", 500000);
 
         System.out.println("Nasabah: " + akun1.getName() + " memiliki saldo: " + "Rp " + akun1.getBalance());
         System.out.println("---------------------------------");
 
-        akun1.deposit(200);
-        akun1.withdraw(100);
+        akun1.deposit(200000);
+        akun1.withdraw(100000);
 
         System.out.println("---------------------------------");
         System.out.println("Nasabah: " + akun1.getName() + " memiliki saldo: " + "Rp " + akun1.getBalance());
