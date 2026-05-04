d5ba5ff88df0502e5866ecefa6e8f5c4f9bf36b4
ReksAstaRaidn <fnoor6612@gmail.com>
2026-05-04 14:50:28 +0700

    Nambah Materi

---
+++ week14/InterfacesAbstractClasses/IntList.java
@@ -0,0 +1,12 @@
+package InterfacesAbstractClasses;
+
+public interface IntList {
+    int size();
+    boolean isEmpty();
+    int get(int index);
+    int indexOf(int value);
+    boolean contains(int value);
+    void add(int value);
+    void add(int index, int value);
+    void remove(int index);    
+}
